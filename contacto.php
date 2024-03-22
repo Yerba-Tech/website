7ae194f0df1f3622a38ac171a88aa4002464a5a9
jpromanonet <programming@jpromano.net>
2024-03-22 16:19:06 -0300

    Working contact

--- contacto.php
+++ contacto.php
@@ -16,14 +16,7 @@ if ($conn->connect_error) {
 
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $fullName = $_POST["fullName"];
-    $email = $_POST["email"];
-    $company = $_POST["company"];
-    $info = $_POST["info"];
-
-    // SQL to insert data into the table
-    $sql = "INSERT INTO contacto (nombre, email, empresa, consulta) VALUES ('$fullName', '$email', '$company', '$info')";
+    
 
     if ($conn->query($sql) === TRUE) {
         // Record inserted successfully, redirect to index.php
